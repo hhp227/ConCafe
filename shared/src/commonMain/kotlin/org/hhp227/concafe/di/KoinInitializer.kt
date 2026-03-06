42db0d244a2d045a54b1be7d3abeabc1ab8cfbb4
hhp227 <hong227@naver.com>
2026-03-06 14:29:12 +0900

    Fix: 컴포즈(Android, Desktop) Koin 잘못 호출되는 코드 수정

--- shared/src/commonMain/kotlin/org/hhp227/concafe/di/KoinInitializer.kt
+++ shared/src/commonMain/kotlin/org/hhp227/concafe/di/KoinInitializer.kt
@@ -2,25 +2,28 @@ package org.hhp227.concafe.di
 
 import org.hhp227.concafe.domain.usecase.HomeUseCaseWrapper
 import org.koin.core.KoinApplication
-import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
+
+private var koinApplication: KoinApplication? = null
 
 fun doInitKoin(): KoinApplication? {
     return doInitKoin(emptyList())
 }
 
 fun doInitKoin(extraModules: List<Module>): KoinApplication? {
-    val current = GlobalContext.getOrNull()
-    return if (current == null) {
+    return if (koinApplication == null) {
         startKoin {
             modules(concafeModules + extraModules)
-        }
+        }.also { koinApplication = it }
     } else {
         null
     }
 }
 
 fun resolveHomeUseCaseWrapper(): HomeUseCaseWrapper {
-    return GlobalContext.get().get()
+    val koin = requireNotNull(koinApplication?.koin) {
+        "Koin is not initialized. Call doInitKoin() before resolving dependencies."
+    }
+    return koin.get()
 }
