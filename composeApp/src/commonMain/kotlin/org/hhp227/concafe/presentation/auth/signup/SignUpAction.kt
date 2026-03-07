ce624c532faffc1dc86db2fc11be559d0c266aea
hhp227 <hong227@naver.com>
2026-03-07 18:43:00 +0900

    Feat: Compose 회원가입 클래스 추가

---
+++ composeApp/src/commonMain/kotlin/org/hhp227/concafe/presentation/auth/signup/SignUpAction.kt
@@ -0,0 +1,4 @@
+package org.hhp227.concafe.presentation.auth.signup
+
+sealed interface SignUpAction {
+}
---
+++ composeApp/src/commonMain/kotlin/org/hhp227/concafe/presentation/auth/signup/SignUpViewModel.kt
@@ -0,0 +1,6 @@
+package org.hhp227.concafe.presentation.auth.signup
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+}
