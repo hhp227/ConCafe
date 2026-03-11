4b91c4c54e9eb638be31caa28fefbf036fe432e0
hhp227 <hong227@naver.com>
2026-03-11 16:52:48 +0900

    Feat: 캐스트 프로필 수정 화면 추가

---
+++ composeApp/src/commonMain/kotlin/org/hhp227/concafe/presentation/castedit/CastEditScreen.kt
@@ -0,0 +1,7 @@
+package org.hhp227.concafe.presentation.castedit
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CastEditScreen() {
+}
---
+++ composeApp/src/commonMain/kotlin/org/hhp227/concafe/presentation/castedit/CastEditViewModel.kt
@@ -0,0 +1,6 @@
+package org.hhp227.concafe.presentation.castedit
+
+import androidx.lifecycle.ViewModel
+
+class CastEditViewModel : ViewModel() {
+}
