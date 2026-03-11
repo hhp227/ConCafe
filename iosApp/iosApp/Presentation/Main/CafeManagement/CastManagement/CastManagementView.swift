c03120bd6a238deb53ef71b35f35d6f00c846b27
hhp227 <hong227@naver.com>
2026-03-11 16:12:24 +0900

    Feat: 스케줄, 캐스트 관리 화면 추가

---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/CastManagement/CastManagementView.swift
@@ -0,0 +1,20 @@
+//
+//  CastManagementView.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import SwiftUI
+
+struct CastManagementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CastManagementView_Previews: PreviewProvider {
+    static var previews: some View {
+        CastManagementView()
+    }
+}
---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/Schedule/ScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleView.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleView()
+    }
+}
