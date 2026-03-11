9a71989c5b6f8879f14530df12ffff2eacd2e0d3
hhp227 <hong227@naver.com>
2026-03-11 12:49:38 +0900

    Feat: 메뉴, 굿즈 수정 화면 파일 추가

---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/MenuGoodsEdit/MenuGoodsEditView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuGoodsEditView.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import SwiftUI
+
+struct MenuGoodsEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuGoodsEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuGoodsEditView()
+    }
+}
---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/MenuGoodsEdit/MenuGoodsEditViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MenuGoodsEditViewModel.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import Foundation
+
+class MenuGoodsEditViewModel: ObservableObject {
+    
+}
