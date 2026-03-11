1e8b27c6ca81a9eaaa48cfcdec878029fbf77a39
hhp227 <hong227@naver.com>
2026-03-11 11:37:46 +0900

    Feat: 카페 공지사항/이벤트 화면

---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/NoticeEvent/NoticeEventView.swift
@@ -0,0 +1,20 @@
+//
+//  NoticeEventView.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import SwiftUI
+
+struct NoticeEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoticeEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoticeEventView()
+    }
+}
