4d366ca12ddb50a6b7fdaaa5d47e5487379cd7c1
hhp227 <hong227@naver.com>
2026-03-11 11:34:03 +0900

    Feat: 메뉴 굿즈 화면 추가

---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/MenuGoods/MenuGoodsUiState.swift
@@ -0,0 +1,12 @@
+//
+//  MenuGoodsUiState.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import Foundation
+
+struct MenuGoodsUiState {
+    
+}
---
+++ iosApp/iosApp/Presentation/Main/CafeManagement/MenuGoods/MenuGoodsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MenuGoodsViewModel.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/11.
+//
+
+import Foundation
+
+class MenuGoodsViewModel: ObservableObject {
+    
+}
