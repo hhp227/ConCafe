b7fe2e910115dba0836a9cb6ef6ba832ce8cb1ae
hhp227 <hong227@naver.com>
2026-03-27 11:00:58 +0900

    Fix: iOS 홈화면에서 배너 코너 잘림 현상

--- iosApp/iosApp/Presentation/Main/Home/HomeView.swift
+++ iosApp/iosApp/Presentation/Main/Home/HomeView.swift
@@ -111,7 +111,7 @@ private struct HomeContentView: View {
                         HomeBannerItem(banner: banner)
                             .frame(maxWidth: .infinity, maxHeight: .infinity)
                             .padding(.horizontal, 16)
-                            .padding(.vertical, 3)
+                            .padding(.vertical, 4)
                             .onTapGesture {
                                 onAction(.bannerTapped(banner))
                             }
@@ -140,7 +140,7 @@ private struct HomeContentView: View {
     }
 
     private var bannerTabViewHeight: CGFloat {
-        bannerHeight + 6
+        bannerHeight + 8
     }
 
     private var popularCastSection: some View {
