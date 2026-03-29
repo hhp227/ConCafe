707e1370a85b3052de753c0f79ad8c8179c1617c
hhp227 <hong227@naver.com>
2026-03-29 11:31:33 +0900

    Feat: RatingBox분리

---
+++ iosApp/iosApp/Presentation/Component/RatingBox.swift
@@ -0,0 +1,20 @@
+//
+//  RatingBox.swift
+//  ConCafe
+//
+//  Created by 홍희표 on 2026/03/29.
+//
+
+import SwiftUI
+
+struct RatingBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RatingBox_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingBox()
+    }
+}
