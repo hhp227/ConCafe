7c64dfe1d43f7afb3cead45d3ade90095b8f7069
hhp227 <hong227@naver.com>
2026-03-29 18:09:54 +0900

    Feat: Compose SplashScreen적용

--- composeApp/src/androidMain/kotlin/com/hhp227/concafe/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/hhp227/concafe/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.hhp227.concafe.presentation.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
--- composeApp/src/jvmMain/kotlin/com/hhp227/concafe/presentation/App.kt
+++ composeApp/src/jvmMain/kotlin/com/hhp227/concafe/presentation/App.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -13,10 +17,12 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.hhp227.concafe.presentation.component.NetworkStatusBanner
 import com.hhp227.concafe.presentation.navigation.NavigationScreen
+import kotlinx.coroutines.delay
 import org.koin.core.context.GlobalContext
 
 @Composable
 fun App() {
+    var isLaunchScreenVisible by remember { mutableStateOf(true) }
     val appViewModel: AppViewModel = viewModel(
         factory = viewModelFactory {
             initializer {
@@ -26,15 +32,23 @@ fun App() {
     )
     val uiState by appViewModel.uiState.collectAsState()
 
+    LaunchedEffect(Unit) {
+        delay(800)
+        isLaunchScreenVisible = false
+    }
     MaterialTheme {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            NavigationScreen()
-            NetworkStatusBanner(
-                networkAlertState = uiState.networkAlertState,
-                modifier = Modifier.align(Alignment.TopCenter)
-            )
+        if (isLaunchScreenVisible) {
+            DesktopLaunchScreen()
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                NavigationScreen()
+                NetworkStatusBanner(
+                    networkAlertState = uiState.networkAlertState,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+            }
         }
     }
 }
