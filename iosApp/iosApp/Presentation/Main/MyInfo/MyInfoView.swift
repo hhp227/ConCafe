654a2cc133212d17c7ccf1873c7d08362a3c6011
hhp227 <hong227@naver.com>
2026-04-16 13:30:31 +0900

    Fix: iOS 활동 뱃지 상태 UI 나오지 않는것

--- iosApp/iosApp/Presentation/Main/MyInfo/MyInfoView.swift
+++ iosApp/iosApp/Presentation/Main/MyInfo/MyInfoView.swift
@@ -852,7 +852,7 @@ private extension View {
 
 private extension CGFloat {
     func clamped(to range: ClosedRange<CGFloat>) -> CGFloat {
-        min(max(self, range.lowerBound), range.upperBound)
+        Swift.min(Swift.max(self, range.lowerBound), range.upperBound)
     }
 }
 
