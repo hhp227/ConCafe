6d7d7ca5bcc1561191f38552a28052b9d8e89c57
hhp227 <hong227@naver.com>
2026-04-18 21:55:21 +0900

    Chore: Compose 버전 업데이트

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -18,7 +18,7 @@ val googleMapsJavascriptApiKey = if (androidGoogleMapsXml.exists()) {
     ""
 }
 
-val appVersionName = "1.07"
+val appVersionName = "1.08"
 val desktopPackageVersion = "1.2.8"
 
 plugins {
@@ -133,7 +133,7 @@ android {
         applicationId = "com.hhp227.concafe"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 11
+        versionCode = 12
         versionName = appVersionName
     }
     buildFeatures {
