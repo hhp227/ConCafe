b08192bc49b102f2c436a95061907649cfd77c6e
hhp227 <hong227@naver.com>
2026-04-21 15:01:50 +0900

    Chore: Compose 데스트톱 버전 업데이트

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -19,7 +19,7 @@ val googleMapsJavascriptApiKey = if (androidGoogleMapsXml.exists()) {
 }
 
 val appVersionName = "1.09"
-val desktopPackageVersion = "1.2.9"
+val desktopPackageVersion = "1.3.0"
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
